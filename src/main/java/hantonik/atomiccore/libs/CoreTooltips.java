5285952c9d6d0a8150c7a39952ffa82bd8a05de5
Piotr Matusiewicz <piotrmatusiewicz85@gmail.com>
2022-01-01 14:19:16 +0100

    Added another tooltip

--- src/main/java/hantonik/atomiccore/libs/CoreTooltips.java
+++ src/main/java/hantonik/atomiccore/libs/CoreTooltips.java
@@ -9,6 +9,7 @@ public class CoreTooltips {
     public static final Tooltip CRAFTING = generate("crafting");
 
     public static final Tooltip HOLD_SHIFT_FOR_INFO = generate("hold_shift_for_info");
+    public static final Tooltip HOLD_ALT_FOR_INFO = generate("hold_alt_for_info");
     public static final Tooltip HOLD_CTRL_FOR_INFO = generate("hold_ctrl_for_info");
 
     public static final TextComponent EMPTY = new TextComponent(" ");
