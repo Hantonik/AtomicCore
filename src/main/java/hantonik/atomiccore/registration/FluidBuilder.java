33fc0e2e97edd8300d8b769f542fc49c5c17337c
Piotr Matusiewicz <piotrmatusiewicz85@gmail.com>
2022-07-04 22:09:56 +0200

    Fixed Fluid registry

--- src/main/java/hantonik/atomiccore/registration/FluidBuilder.java
+++ src/main/java/hantonik/atomiccore/registration/FluidBuilder.java
@@ -1,86 +1,84 @@
 package hantonik.atomiccore.registration;
 
-// TODO
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
 
-//import net.minecraft.world.item.Item;
-//import net.minecraft.world.level.block.LiquidBlock;
-//import net.minecraft.world.level.material.Fluid;
-//import net.minecraftforge.fluids.FluidAttributes;
-//import net.minecraftforge.fluids.ForgeFlowingFluid;
-//
-//import java.util.function.Supplier;
-//
-//public class FluidBuilder {
-//    private Supplier<? extends Item> bucket;
-//    private Supplier<? extends LiquidBlock> block;
-//
-//    private final FluidAttributes.Builder attributes;
-//
-//    private boolean canMultiply = false;
-//    private float explosionResistance = 1.0F;
-//
-//    private int slopeFindDistance = 4;
-//    private int levelDecreasePerBlock = 1;
-//    private int tickRate = 5;
-//
-//    public FluidBuilder canMultiply() {
-//        this.canMultiply = true;
-//
-//        return this;
-//    }
-//
-//    public ForgeFlowingFluid.Properties build(Supplier<? extends Fluid> still, Supplier<? extends Fluid> flowing) {
-//        ForgeFlowingFluid.Properties properties = (new ForgeFlowingFluid.Properties(still, flowing, this.attributes)).slopeFindDistance(this.slopeFindDistance).levelDecreasePerBlock(this.levelDecreasePerBlock).explosionResistance(this.explosionResistance).tickRate(this.tickRate).block(this.block).bucket(this.bucket);
-//
-//        if (this.canMultiply)
-//            properties.canMultiply();
-//
-//        return properties;
-//    }
-//
-//    public FluidBuilder canMultiply(boolean canMultiply) {
-//        this.canMultiply = canMultiply;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder bucket(Supplier<? extends Item> bucket) {
-//        this.bucket = bucket;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder block(Supplier<? extends LiquidBlock> block) {
-//        this.block = block;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder slopeFindDistance(int slopeFindDistance) {
-//        this.slopeFindDistance = slopeFindDistance;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder levelDecreasePerBlock(int levelDecreasePerBlock) {
-//        this.levelDecreasePerBlock = levelDecreasePerBlock;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder explosionResistance(float explosionResistance) {
-//        this.explosionResistance = explosionResistance;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder tickRate(int tickRate) {
-//        this.tickRate = tickRate;
-//
-//        return this;
-//    }
-//
-//    public FluidBuilder(FluidAttributes.Builder attributes) {
-//        this.attributes = attributes;
-//    }
-//}
+import java.util.function.Supplier;
+
+public class FluidBuilder {
+    private Supplier<? extends Item> bucket;
+    private Supplier<? extends LiquidBlock> block;
+
+    private final FluidType.Properties properties;
+
+    private boolean canConvertToSource = false;
+    private float explosionResistance = 1.0F;
+
+    private int slopeFindDistance = 4;
+    private int levelDecreasePerBlock = 1;
+    private int tickRate = 5;
+
+    public FluidBuilder canConvertToSource() {
+        this.canConvertToSource = true;
+
+        return this;
+    }
+
+    public ForgeFlowingFluid.Properties build(Supplier<? extends Fluid> still, Supplier<? extends Fluid> flowing) {
+        ForgeFlowingFluid.Properties properties = (new ForgeFlowingFluid.Properties(() -> new FluidType(this.properties), still, flowing)).slopeFindDistance(this.slopeFindDistance).levelDecreasePerBlock(this.levelDecreasePerBlock).explosionResistance(this.explosionResistance).tickRate(this.tickRate).block(this.block).bucket(this.bucket);
+
+        if (this.canConvertToSource)
+            this.properties.canConvertToSource(this.canConvertToSource);
+
+        return properties;
+    }
+
+    public FluidBuilder canConvertToSource(boolean canConvertToSource) {
+        this.canConvertToSource = canConvertToSource;
+
+        return this;
+    }
+
+    public FluidBuilder bucket(Supplier<? extends Item> bucket) {
+        this.bucket = bucket;
+
+        return this;
+    }
+
+    public FluidBuilder block(Supplier<? extends LiquidBlock> block) {
+        this.block = block;
+
+        return this;
+    }
+
+    public FluidBuilder slopeFindDistance(int slopeFindDistance) {
+        this.slopeFindDistance = slopeFindDistance;
+
+        return this;
+    }
+
+    public FluidBuilder levelDecreasePerBlock(int levelDecreasePerBlock) {
+        this.levelDecreasePerBlock = levelDecreasePerBlock;
+
+        return this;
+    }
+
+    public FluidBuilder explosionResistance(float explosionResistance) {
+        this.explosionResistance = explosionResistance;
+
+        return this;
+    }
+
+    public FluidBuilder tickRate(int tickRate) {
+        this.tickRate = tickRate;
+
+        return this;
+    }
+
+    public FluidBuilder(FluidType.Properties properties) {
+        this.properties = properties;
+    }
+}
--- src/main/java/hantonik/atomiccore/registration/FluidDeferredRegisterExtension.java
+++ src/main/java/hantonik/atomiccore/registration/FluidDeferredRegisterExtension.java
@@ -1,21 +1,19 @@
 package hantonik.atomiccore.registration;
 
-// TODO
+import hantonik.atomiccore.registration.deferred.FluidDeferredRegister;
+import hantonik.atomiccore.registration.object.FluidObject;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.fluids.FluidType;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
 
-//import hantonik.atomiccore.registration.deferred.FluidDeferredRegister;
-//import hantonik.atomiccore.registration.object.FluidObject;
-//import net.minecraft.world.level.material.Material;
-//import net.minecraftforge.fluids.FluidAttributes;
-//import net.minecraftforge.fluids.ForgeFlowingFluid;
-//
-//import java.util.function.Function;
-//
-//public class FluidDeferredRegisterExtension extends FluidDeferredRegister {
-//    public FluidDeferredRegisterExtension(String modID) {
-//        super(modID);
-//    }
-//
-//    public <F extends ForgeFlowingFluid> FluidObject<F> register(String name, String tagName, FluidAttributes.Builder builder, Function<ForgeFlowingFluid.Properties, ? extends F> still, Function<ForgeFlowingFluid.Properties, ? extends F> flowing, Material material, int lightLevel) {
-//        return super.register(name, tagName, builder.luminosity(lightLevel), still, flowing, material, lightLevel);
-//    }
-//}
+import java.util.function.Function;
+
+public class FluidDeferredRegisterExtension extends FluidDeferredRegister {
+    public FluidDeferredRegisterExtension(String modID) {
+        super(modID);
+    }
+
+    public <F extends ForgeFlowingFluid> FluidObject<F> register(String name, String tagName, FluidType.Properties properties, Function<ForgeFlowingFluid.Properties, ? extends F> still, Function<ForgeFlowingFluid.Properties, ? extends F> flowing, Material material, int lightLevel) {
+        return super.register(name, tagName, properties.lightLevel(lightLevel), still, flowing, material, lightLevel);
+    }
+}
